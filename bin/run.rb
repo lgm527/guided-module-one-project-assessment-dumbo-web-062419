a806597b93b1b2b6146d4a819d6a9c49417a9604
TeenageGrampa <danielhvoigt@gmail.com>
2019-07-08 17:52:27 -0400

    added user story in comments of run

--- bin/run.rb
+++ bin/run.rb
@@ -1,4 +1,12 @@
 require_relative '../config/environment'
 
+#Greeting
 
+#prompt: create user or use existing
+
+#new user: enters name User.new -> create schedule
+
+#Existing user: new schedule, view schedule, edit schedule, or delete schedule
+
+#New schedule gives a prompt to pick between artists playing at a certain time. Once you pick your artists for each conflicting time frame it creates a new schedule
 
